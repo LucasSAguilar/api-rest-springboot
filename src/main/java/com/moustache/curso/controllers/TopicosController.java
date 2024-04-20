ed06ab0c2556637f3c9b55230d623d70275634da
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-04-20 16:46:05 -0300

    Boas praticas em REST de retorno

--- src/main/java/com/moustache/curso/controllers/TopicosController.java
+++ src/main/java/com/moustache/curso/controllers/TopicosController.java
@@ -1,12 +1,17 @@
 package com.moustache.curso.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.moustache.curso.dto.TopicoDto;
 import com.moustache.curso.form.TopicoForm;
@@ -51,9 +56,13 @@ public class TopicosController {
     }
 
     @PostMapping
-    public void cadastrar(TopicoForm form) {
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.formatarParaTopico(cursoRepository);
         topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+
     }
 
 }
